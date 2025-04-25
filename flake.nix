377a895688bbd301f3de3e6e2f0213dd65b816f7
li <li@maisiliym.criome.me>
2025-04-25 12:01:10 -0400

    implement(mkAtomFlake registry)

--- flake.nix
+++ flake.nix
@@ -28,13 +28,11 @@
           args = { inherit atomSrc; };
           system = inputs.system.value;
           _calledFromFlake = true;
-          local-registry = (
-            removeAttrs inputs [
-              "self"
-              "system"
-              "make-atom"
-            ]
-          );
+          registry = removeAttrs inputs [
+            "self"
+            "system"
+            "make-atom"
+          ];
         };
 
       tests = import ./tests.nix;
