13a58c2cc71027004e1859ab845adcbeb7a6bafc
li <li@maisiliym.criome.me>
2025-05-04 11:49:58 -0400

    improve(flake registry)

--- atom-nix/Registry.nix
+++ atom-nix/Registry.nix
@@ -1,1 +1,6 @@
-get.parentRegistry // registry // mod.localRegistry
+# With flakes, inputs passed directly must have priority to allow overrides
+
+if get._calledFromFlake then
+  get.parentRegistry // mod.localRegistry // registry
+else
+  get.parentRegistry // registry // mod.localRegistry
